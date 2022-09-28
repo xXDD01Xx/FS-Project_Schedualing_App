afe9bec325851e73d47183a623acf13a31d9708a
Daniel Danio <aspirexinspire@gmail.com>
2022-09-28 12:52:24 -0400

    Updating for merge, added component ModCO

--- capstone/vue/src/views/LandingPage.vue
+++ capstone/vue/src/views/LandingPage.vue
@@ -1,19 +1,25 @@
 <template>
   <v-app id="app">
     <v-container id="landing-container">
-      <router-link :to="{path: '/login'}">
+
+      <h1 id="introduction">
+        Welcome to the Frontline Project Schedule App
+      </h1>
+      <v-col class="text-right">
+      <router-link class="text-decoration-none" :to="{path: '/login'}">
         <v-btn class="button"
         color=#76a1c0
         elevation="2"
         outlined>Login</v-btn>
       </router-link>
       <br>
-      <router-link :to="{path: '/register'}">
+      <router-link class="text-decoration-none" :to="{path: '/register'}">
         <v-btn class="button"
         color=#76a1c0
         elevation="2"
         outlined>Register</v-btn>
       </router-link>
+      </v-col>
     </v-container>
     <v-container>
       <!-- <v-img id="logo" src="../assets/flag-logo.svg"/> -->
@@ -31,10 +37,11 @@ export default {
 #app{
   background: url('../assets/hero-1.jpeg') no-repeat center center fixed !important;
   background-repeat: no-repeat;
-  background-size: cover;
-}
-/* #logo {
+  background-size: cover;}
 
-} */
+#introduction {
+  color: white;
+}
+
 
 </style>

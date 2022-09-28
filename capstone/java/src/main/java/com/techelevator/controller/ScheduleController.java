ed9571b913ef5d10c0e5f7bd97a7427806640f28
Jason Belmana <jbelmana@gmail.com>
2022-09-28 17:24:09 -0400

    ScheduleController

--- capstone/java/src/main/java/com/techelevator/controller/ScheduleController.java
+++ capstone/java/src/main/java/com/techelevator/controller/ScheduleController.java
@@ -2,7 +2,6 @@ package com.techelevator.controller;
 
 import com.techelevator.dao.ScheduleDao;
 import com.techelevator.model.Project;
-import com.techelevator.dao.ProjectDAO;
 import com.techelevator.model.Schedule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -19,9 +18,18 @@ public class ScheduleController {
     @Autowired
     ScheduleDao scheduleDao;
 
+    @RequestMapping(path = "/project/{id}/Description", method = RequestMethod.GET)
+    public Integer projectIdFromProjectName(@PathVariable String projectName) {
+        return scheduleDao.projectIdFromProjectName(projectName);
+    }
 
-    @RequestMapping(path = "/schedule", method = RequestMethod.GET)
-    public List<Schedule> listScheduleItems(int projectId) {
+    @RequestMapping(path = "/phase/{id}/Description", method = RequestMethod.GET)
+    public Integer phaseIdFromPhaseDescription(@PathVariable String description) {
+        return scheduleDao.phaseIdFromPhaseDescription(description);
+    }
+
+    @RequestMapping(path = "/schedule/{id}", method = RequestMethod.GET)
+    public List<Schedule> listScheduleItems(@PathVariable int projectId) {
         return scheduleDao.listScheduleItems(projectId);
     }
 
@@ -30,12 +38,12 @@ public class ScheduleController {
         scheduleDao.addToSchedule(project, schedule);
     }
 
-    @RequestMapping(path = "schedule/update", method = RequestMethod.PUT)
+    @RequestMapping(path = "/schedule/update", method = RequestMethod.PUT)
     public void updateScheduleItem(@RequestBody Schedule schedule) {
         scheduleDao.updateScheduleItem(schedule);
     }
 
-    @RequestMapping(path = "schedule/{id}/delete", method = RequestMethod.DELETE)
+    @RequestMapping(path = "/schedule/{id}/delete", method = RequestMethod.DELETE)
     public void deleteProject(@PathVariable int id) {
         scheduleDao.deleteScheduleItem(id);
     }

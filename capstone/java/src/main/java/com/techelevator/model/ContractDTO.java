461e1ae58dcedef7984a51719705a715f2ef00b1
Chris <chris@c-metzger.com>
2022-09-28 16:42:52 -0400

    Start list of contracts

---
+++ capstone/java/src/main/java/com/techelevator/model/ContractDTO.java
@@ -0,0 +1,23 @@
+package com.techelevator.model;
+
+public class ContractDTO {
+    public int id;
+    public String contractName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContractName() {
+        return contractName;
+    }
+
+    public void setContractName(String contractName) {
+        this.contractName = contractName;
+    }
+
+}

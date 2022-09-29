d520ee52539e66a603685b5216657fca40963767
Daniel Danio <aspirexinspire@gmail.com>
2022-09-29 13:59:00 -0400

    Updating for pull

---
+++ capstone/vue/src/views/EditProject.vue
@@ -0,0 +1,26 @@
+<template>
+<v-app>
+  <v-container>
+  <p>{{ this.$store.state.project }}</p>
+
+
+  </v-container>
+  <router-link class="text-decoration-none" :to="{path: '/home'}">
+    <v-btn class="button"
+           color=#8c090e
+           elevation="2"
+           outlined
+    >Home</v-btn>
+  </router-link>
+</v-app>
+</template>
+
+<script>
+export default {
+  name: "EditProject",
+}
+</script>
+
+<style scoped>
+
+</style>

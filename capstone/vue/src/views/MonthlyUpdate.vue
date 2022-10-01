92fe906acf3e6852c35a168c67682b0e0c208bb1
Andrew Briss <adbriss@gmail.com>
2022-10-01 11:05:32 -0400

    Started on MonthlyUpdate

--- capstone/vue/src/views/MonthlyUpdate.vue
+++ capstone/vue/src/views/MonthlyUpdate.vue
@@ -1,18 +1,87 @@
 <template>
   <v-app>
-    <router-link class="text-decoration-none" :to="{path: '/home'}">
-      <v-btn class="button"
-              color=#8c090e
-              elevation="2"
-              outlined
-              >Home</v-btn>
-    </router-link>
+      <v-container>
+        <br>
+        <h2 class="text-center">Monthly Updates</h2>
+        <br><br>
+      <v-simple-table>
+        <thead>
+        <th>Project Name</th>
+        <th>Date Received</th>
+        <th>Last Modified</th>
+        <th>Budget</th>
+        <th>Construction Tasks</th>
+        <th>Substantial Tasks</th>
+        <th>Add Monthly Update</th>
+        </thead>
+        <tbody>
+        <tr v-for="project in projects" :key="project.id">
+          <td>
+            {{ project.projectName }}
+          </td>
+          <td>
+            {{ project.dateReceived }}
+          </td>
+          <td>
+            {{ project.lastModified }}
+          </td>
+          <td>
+            {{ project.budget }}
+          </td>
+          <td>
+            {{ project.tasksConstruction }}
+          </td>
+          <td>
+            {{ project.tasksSubstantial }}
+          </td>
+          <td>
+            <v-btn 
+            elevation="2" class="text-decoration-none" 
+            @click="monthlyUpdate(project)">Add Update</v-btn>
+          </td>
+        </tr>
+        </tbody>
+      </v-simple-table>
+    </v-container>
+    <div class="text-center">
+      <br><br>
+      <router-link class="text-decoration-none" :to="{path: '/home'}">
+        <v-btn class="button"
+               color=#8c090e
+               elevation="2"
+               outlined
+        >Home</v-btn>
+      </router-link>
+    </div>
   </v-app>
 </template>
 
 <script>
-export default {
+import ProjectService from "@/services/ProjectService";
 
+export default {
+    name: 'MonthlyUpdate',
+    data(){
+      return {
+        projects: [],
+        showEdit: false,
+      }
+  },
+  created(){
+    ProjectService.listProjects().then((response) =>{
+      if (response.status == 200 || response.status == 201)
+      {
+        this.projects = response.data;
+      }
+      //TODO filter?
+    });
+  },
+  methods: {
+    monthlyUpdate(project){
+      this.$store.commit('SET_PROJECT', project);
+      this.$router.push({name: 'MonthlyUpdateById', params: {id: project.id}})
+    }
+  }
 }
 </script>
 
---
+++ capstone/vue/src/views/MonthlyUpdateById.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app>
+      
+        <template>
+            <v-row justify="center">
+                <v-date-picker
+                v-model="picker"
+                type="month"
+                ></v-date-picker>
+            </v-row>
+        </template>
+
+
+
+
+
+
+
+
+
+      <router-link class="text-decoration-none" :to="{path: '/monthlyUpdate'}">
+        <v-btn class="button"
+               color=#8c090e
+               elevation="2"
+               outlined
+        >Back</v-btn>
+      </router-link>
+      <router-link class="text-decoration-none" :to="{path: '/home'}">
+        <v-btn class="button"
+               color=#8c090e
+               elevation="2"
+               outlined
+        >Home</v-btn>
+      </router-link>
+  </v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

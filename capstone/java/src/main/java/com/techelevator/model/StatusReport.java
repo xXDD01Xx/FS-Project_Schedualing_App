170b35b0fdb79924728e8f3ed7effb12377ac6e1
Jason Belmana <jbelmana@gmail.com>
2022-10-03 15:23:07 -0400

    added columns

--- capstone/java/src/main/java/com/techelevator/model/StatusReport.java
+++ capstone/java/src/main/java/com/techelevator/model/StatusReport.java
@@ -14,10 +14,13 @@ public class StatusReport {
     private LocalDate monthYear;
     private String phase;
     private String itemDescription;
+    private String schedule_notes;
+    private int pct_complete;
+    private Float schedProdIdx;
 
     public StatusReport(){}
 
-    public StatusReport(String contractName, int contractId, String projectName, int projectId, int phaseItem, LocalDate itemDate, int itemTasks, LocalDate monthYear, String phase, String itemDescription) {
+    public StatusReport(String contractName, int contractId, String projectName, int projectId, int phaseItem, LocalDate itemDate, int itemTasks, LocalDate monthYear, String phase, String itemDescription, String schedule_notes, int pct_complete, Float schedProdIdx) {
         this.contractName = contractName;
         this.contractId = contractId;
         this.projectName = projectName;
@@ -28,6 +31,9 @@ public class StatusReport {
         this.monthYear = monthYear;
         this.phase = phase;
         this.itemDescription = itemDescription;
+        this.schedule_notes = schedule_notes;
+        this.pct_complete = pct_complete;
+        this.schedProdIdx = schedProdIdx;
     }
 
     public String getContractName() {
@@ -108,5 +114,29 @@ public class StatusReport {
 
     public void setItemDescription(String itemDescription) {
         this.itemDescription = itemDescription;
+    }
+
+    public String getSchedule_notes() {
+        return schedule_notes;
+    }
+
+    public void setSchedule_notes(String schedule_notes) {
+        this.schedule_notes = schedule_notes;
+    }
+
+    public int getPct_complete() {
+        return pct_complete;
+    }
+
+    public void setPct_complete(int pct_complete) {
+        this.pct_complete = pct_complete;
+    }
+
+    public Float getSchedProdIdx() {
+        return schedProdIdx;
+    }
+
+    public void setSchedProdIdx(Float schedProdIdx) {
+        this.schedProdIdx = schedProdIdx;
     }
 }

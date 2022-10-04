2b8a2ea4849085e51750e4ffff9a7d6f3895db78
Chris <chris@c-metzger.com>
2022-10-04 15:18:06 -0400

    all baseline items on init load

--- capstone/java/database/data/scheduleData.sql
+++ capstone/java/database/data/scheduleData.sql
@@ -1,4 +1,5 @@
 begin;
+truncate baseline_sched_items, monthly_sched_items, monthly_schedule;
 insert into monthly_schedule
 (id, project_id, month_year, schedule_notes, pct_complete)
 values
@@ -7,51 +8,34 @@ values
 insert into monthly_sched_items
 (monthly_sched_id, phase_item, item_date, item_tasks)
 values
-(1,1,'2022-01-01',1),
-(1,6,'2022-01-31',2),
-(1,7,'2022-02-01',3),
-(1,12,'2022-02-28',4),
-(1,13,'2022-03-01',5),
-(1,17,'2022-03-31',6);
+(1,1,'2022-09-01',1),
+(1,6,'2022-09-30',2),
+(1,7,'2022-10-01',3),
+(1,12,'2022-10-28',4),
+(1,13,'2022-10-29',5),
+(1,17,'2022-12-31',6);
 
 insert into baseline_sched_items
 (project_id, phase_item, item_date, item_tasks)
 values
-(2,1,'2022-01-01',1),
-(2,6,'2022-01-31',2),
-(2,7,'2022-02-01',3),
-(2,12,'2022-02-28',4),
-(2,13,'2022-03-01',5),
-(2,17,'2022-03-31',6);
+(1,1,'2022-09-01',1),
+(1,6,'2022-09-30',2),
+(1,7,'2022-10-01',3),
+(1,12,'2022-10-28',4),
+(1,13,'2022-10-29',5),
+(1,17,'2023-01-31',6),
+(2,1,'2022-09-01',1),
+(2,6,'2022-09-30',2),
+(2,7,'2022-10-01',3),
+(2,12,'2022-10-31',4),
+(2,13,'2022-11-01',5),
+(2,17,'2022-12-31',6);
+
 insert into baseline_sched_items
 (project_id, phase_item)
-values
-(2,2),
-(2,3),
-(2,4),
-(2,5),
-(2,8),
-(2,9),
-(2,10),
-(2,11),
-(2,14),
-(2,15),
-(2,16),
-(1,1),
-(1,2),
-(1,3),
-(1,4),
-(1,5),
-(1,6),
-(1,7),
-(1,8),
-(1,9),
-(1,10),
-(1,11),
-(1,12),
-(1,13),
-(1,14),
-(1,15),
-(1,16),
-(1,17);
+select p.id, pi.id
+from project p, phase_items pi
+group by p.id, pi.id
+having (select count(*) from baseline_sched_items b where b.project_id=p.id and b.phase_item=pi.id)<1
+order by p.id, pi.id;
 commit;

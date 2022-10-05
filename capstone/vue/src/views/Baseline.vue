4b90c25a2f370a818576577b70348a2ef2e7e283
Chris <chris@c-metzger.com>
2022-10-04 20:18:42 -0400

    minor formatting

--- capstone/vue/src/views/Baseline.vue
+++ capstone/vue/src/views/Baseline.vue
@@ -1,90 +1,102 @@
 <template>
   <v-app>
-      <v-container>
-        <br>
-        <h2 class="text-center">List Baseline</h2>
-        <br><br>
+    <v-container>
+      <br />
+      <h2 class="text-center">List Baseline</h2>
+      <br /><br />
       <v-simple-table>
-        <thead style="display:table-header-group;" >
-        <tr>
-          <th>Project Name</th>
-        <th>Date Received</th>
-        <th>Last Modified</th>
-        <th>Budget</th>
-        <th>Construction Tasks</th>
-        <th>Substantial Tasks</th>
-        <th>View Baseline Items</th>
-        </tr>
+        <thead style="display:table-header-group;">
+          <tr>
+            <th>Project Name</th>
+            <th>Date Received</th>
+            <th>Last Modified</th>
+            <th>Budget</th>
+            <th>Construction Tasks</th>
+            <th>Substantial Tasks</th>
+            <th>View Baseline Items</th>
+          </tr>
         </thead>
         <tbody>
-        <tr v-for="project in projects" :key="project.id">
-          <td>
-            {{ project.projectName }}
-          </td>
-          <td>
-            {{ new Date(project.dateReceived + " 05:00:00").toLocaleDateString(`en-US`, {dateStyle:"short"}) }}
-          </td>
-          <td>
-            {{ new Date(project.lastModified).toLocaleString(`en-US`, opts) }}
-          </td>
-          <td>
-            {{ project.budget.toLocaleString("en-US", {style:"currency", currency:"USD"}) }}
-          </td>
-          <td>
-            {{ project.tasksConstruction }}
-          </td>
-          <td>
-            {{ project.tasksSubstantial }}
-          </td>
-          <td>
-            <v-btn 
-            elevation="2" class="text-decoration-none" 
-            @click="editProject(project)">Edit Baseline</v-btn>
-          </td>
-        </tr>
+          <tr v-for="project in projects" :key="project.id">
+            <td>
+              {{ project.projectName }}
+            </td>
+            <td>
+              {{
+                new Date(
+                  project.dateReceived + " 05:00:00"
+                ).toLocaleDateString(`en-US`, { dateStyle: "short" })
+              }}
+            </td>
+            <td>
+              {{ new Date(project.lastModified).toLocaleString(`en-US`, opts) }}
+            </td>
+            <td>
+              {{
+                project.budget.toLocaleString("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                })
+              }}
+            </td>
+            <td>
+              {{ project.tasksConstruction }}
+            </td>
+            <td>
+              {{ project.tasksSubstantial }}
+            </td>
+            <td>
+              <v-btn
+                elevation="2"
+                class="text-decoration-none"
+                @click="editProject(project)"
+                >Edit Baseline</v-btn
+              >
+            </td>
+          </tr>
         </tbody>
       </v-simple-table>
     </v-container>
     <div class="text-center">
-      <br><br>
-      <router-link class="text-decoration-none" :to="{path: '/home'}">
-        <v-btn class="button"
-               color=#8c090e
-               elevation="2"
-               outlined
-        >Home</v-btn>
+      <br /><br />
+      <router-link class="text-decoration-none" :to="{ path: '/home' }">
+        <v-btn class="button" color="#8c090e" elevation="2" outlined
+          >Home</v-btn
+        >
       </router-link>
     </div>
   </v-app>
 </template>
 
 <script>
-import ProjectService from '../services/ProjectService.js'
+  import ProjectService from "../services/ProjectService.js";
 
-export default {
-    name: 'Baseline',
-    data(){
-        return {
+  export default {
+    name: "Baseline",
+    data() {
+      return {
         projects: [],
-        }
+        opts: { dateStyle: "short", timeStyle: "short" },
+      };
     },
-    created(){
-        ProjectService.listProjects().then((response) =>{
-            if (response.status == 200 || response.status == 201){
-                this.projects = response.data;
-            }
-            //TODO filter?
-            });
+    created() {
+      ProjectService.listProjects().then((response) => {
+        if (response.status == 200 || response.status == 201) {
+          this.projects = response.data;
+        }
+        //TODO filter?
+      });
     },
     methods: {
-        editProject(project){
-        this.$store.commit('SET_PROJECT', project);
-        this.$router.push({name: 'BaselineSchedule', params: {id: project.id}})
-        }
-  }
-}
+      editProject(project) {
+        this.$store.commit("SET_PROJECT", project);
+        this.$router.push({
+          name: "BaselineSchedule",
+          params: { id: project.id },
+        });
+      },
+    },
+  };
 </script>
 
-<style>
-
-</style>
+<style></style>
--- capstone/vue/src/views/ListBaselineItems.vue
+++ capstone/vue/src/views/ListBaselineItems.vue
@@ -1,61 +1,72 @@
 <template>
   <v-app>
-      <v-container>
-        <br>
-        <h2 class="text-center">List Baseline</h2>
-        <br><br>
+    <v-container>
+      <br />
+      <h2 class="text-center">List Baseline</h2>
+      <br /><br />
       <v-simple-table>
-        <thead style="display:table-header-group;" >
-        <tr>
-          <th>Project Name</th>
-        <th>Date Received</th>
-        <th>Last Modified</th>
-        <th>Budget</th>
-        <th>Construction Tasks</th>
-        <th>Substantial Tasks</th>
-        <th>View Baseline Items</th>
-        </tr>
+        <thead style="display:table-header-group;">
+          <tr>
+            <th>Project Name</th>
+            <th>Date Received</th>
+            <th>Last Modified</th>
+            <th>Budget</th>
+            <th>Construction Tasks</th>
+            <th>Substantial Tasks</th>
+            <th>View Baseline Items</th>
+          </tr>
         </thead>
         <tbody>
-        <tr v-for="project in projects" :key="project.id">
-          <td>
-            {{ project.projectName }}
-          </td>
-          <td>
-            {{ new Date(project.dateReceived + " 05:00:00").toLocaleDateString(`en-US`, {dateStyle:"short"}) }}
-          </td>
-          <td>
-            {{ new Date(project.lastModified).toLocaleString(`en-US`, opts) }}
-          </td>
-          <td>
-            {{ project.budget.toLocaleString("en-US", {style:"currency", currency:"USD"}) }}
-          </td>
-          <td>
-            {{ project.tasksConstruction }}
-          </td>
-          <td>
-            {{ project.tasksSubstantial }}
-          </td>
-          <td>
-            <v-btn 
-            elevation="2" class="text-decoration-none" 
-            @click="editProject(project)">View</v-btn>
-          </td>
-        </tr>
+          <tr v-for="project in projects" :key="project.id">
+            <td>
+              {{ project.projectName }}
+            </td>
+            <td>
+              {{
+                new Date(
+                  project.dateReceived + " 05:00:00"
+                ).toLocaleDateString(`en-US`, { dateStyle: "short" })
+              }}
+            </td>
+            <td>
+              {{ new Date(project.lastModified).toLocaleString(`en-US`, opts) }}
+            </td>
+            <td>
+              {{
+                project.budget.toLocaleString("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                })
+              }}
+            </td>
+            <td>
+              {{ project.tasksConstruction }}
+            </td>
+            <td>
+              {{ project.tasksSubstantial }}
+            </td>
+            <td>
+              <v-btn
+                elevation="2"
+                class="text-decoration-none"
+                @click="editProject(project)"
+                >View</v-btn
+              >
+            </td>
+          </tr>
         </tbody>
       </v-simple-table>
     </v-container>
     <div class="text-center">
-      <br><br>
-      <router-link
-          class="text-decoration-none"
-          :to="{ path: '/home' }">
+      <br /><br />
+      <router-link class="text-decoration-none" :to="{ path: '/home' }">
         <v-btn class="button" color="#8c090e" elevation="2" outlined
-        >Back</v-btn>
+          >Back</v-btn
+        >
       </router-link>
       <router-link class="text-decoration-none" :to="{ path: '/home' }">
         <v-btn class="button" color="#8c090e" elevation="2" outlined
-        >Home</v-btn
+          >Home</v-btn
         >
       </router-link>
     </div>
@@ -63,34 +74,35 @@
 </template>
 
 <script>
-import ProjectService from "@/services/ProjectService";
+  import ProjectService from "@/services/ProjectService";
 
-export default {
-    name: 'ListBaselineItems',
-    data(){
+  export default {
+    name: "ListBaselineItems",
+    data() {
       return {
         projects: [],
         showEdit: false,
-      }
-  },
-  created(){
-    ProjectService.listProjects().then((response) =>{
-      if (response.status == 200 || response.status == 201)
-      {
-        this.projects = response.data;
-      }
-      //TODO filter?
-    });
-  },
-  methods: {
-    editProject(project){
-      this.$store.commit('SET_PROJECT', project);
-      this.$router.push({name: 'ListBaselineByID', params: {id: project.id}})
-    }
-  }
-}
+        opts: { dateStyle: "short", timeStyle: "short" },
+      };
+    },
+    created() {
+      ProjectService.listProjects().then((response) => {
+        if (response.status == 200 || response.status == 201) {
+          this.projects = response.data;
+        }
+        //TODO filter?
+      });
+    },
+    methods: {
+      editProject(project) {
+        this.$store.commit("SET_PROJECT", project);
+        this.$router.push({
+          name: "ListBaselineByID",
+          params: { id: project.id },
+        });
+      },
+    },
+  };
 </script>
 
-<style>
-
-</style>
+<style></style>
